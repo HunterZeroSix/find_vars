b82ed210d1ffcb10c33c5dc150ce3f4591bb7fa7
HunterZeroSix <lukasrettler96@gmail.com>
2020-11-06 13:42:51 +0100

    optimize incoming sep_end

--- find_vars.py
+++ find_vars.py
@@ -7,24 +7,23 @@ def find_vars(msg, sep, sep_end=None):
 
     str(msg), str(sep), sep_end default as None else as str(sep_end)  
     '''
-    if sep_end == None: # setup message counter
-        sep_count = int(msg.count(sep)/2)
-        sep_end = sep
-    elif sep_end == sep:
-        sep_count = int(msg.count(sep)/2)
-        sep_end = sep
+    # setup message counter
+    if (sep_end == None) or (sep_end == sep): 
+        sep_count, sep_end = int(msg.count(sep)/2), sep
+    elif (sep_end == "") or (sep_end == ''):
+        raise ValueError('sep_end can not be a empty string')
     else: sep_count = int(msg.count(sep))
     
-    t = 0 # Init Startpoint
+    t = 0 # Init startpoint
     found_list = [] # Init list
     for i in range(0,sep_count):
         found_start = msg.find(sep, t) + len(sep) # found beginning of the var
         found_end = msg.find(sep_end, found_start) # found end of the var
         found_var = msg[found_start:found_end] # return var between the sep
-        if found_var not in found_list: # if founded var is not in list
-            found_list.append(found_var) # add to list founded vars
+        if found_var not in found_list: # if found_var is not in list
+            found_list.append(found_var) # add to list
         t = found_end + len(sep_end)
         # print("Variable", i+1, "|", found_var)
     from contextlib import redirect_stdout 
     with redirect_stdout(None): print(i)
-    return found_list # return 
+    return found_list # return list
